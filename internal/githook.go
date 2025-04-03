db774cefe31de2e941a73070d32bc9024d6ae611
Patricio Ascencio <patricio.ascencio@karibu.cl>
2025-04-02 21:38:49 -0300

    feat: Agregar comando para crear un githook pre-commit en Linux y Windows, incluyendo la lógica para establecer permisos de ejecución y manejo de errores.

---
+++ internal/githook.go
@@ -0,0 +1,64 @@
+package internal
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/gookit/slog"
+	"github.com/spf13/cobra"
+)
+
+const preCommitHookLinux = `#!/bin/sh
+cwd=$(pwd)
+tvosec scan --path $cwd --staged
+`
+
+const preCommitHookWindows = `@echo off
+set cwd=%cd%
+tvosec scan --path %cwd% --staged
+`
+
+func NewGitHookCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "githook",
+		Run: gitHook,
+	}
+	return cmd
+}
+
+func gitHook(cmd *cobra.Command, args []string) {
+	slog.Info("Creating git hook...")
+	hookPath := filepath.Join(".git", "hooks")
+	if _, err := os.Stat(hookPath); os.IsNotExist(err) {
+		slog.Error("hook directory not found")
+		return
+	}
+	hookPath = filepath.Join(hookPath, "pre-commit")
+	os.MkdirAll(filepath.Dir(hookPath), 0755)
+	if runtime.GOOS == "windows" {
+		fileName := strings.Join([]string{hookPath, "bat"}, ".")
+		err := os.WriteFile(fileName, []byte(preCommitHookWindows), 0755)
+		if err != nil {
+			slog.Error("Error al crear el archivo:", err)
+			return
+		}
+		slog.Info("Githook creado:", fileName)
+	} else {
+		fileName := hookPath
+		err := os.WriteFile(fileName, []byte(preCommitHookLinux), 0755) // Execute permission
+		if err != nil {
+			slog.Error("Error al crear el archivo:", err)
+			return
+		}
+
+		// Asegurar permisos de ejecución en Linux
+		err = os.Chmod(fileName, 0755)
+		if err != nil {
+			slog.Error("Error al establecer permisos de ejecución:", err)
+			return
+		}
+		slog.Info("Githook creado:", fileName)
+	}
+}
